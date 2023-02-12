ffa1748e5d5482bf529e7a677ccd12cc0ea53b7a
Ekete Emmanuel <eketeeu@gmail.com>
2023-02-12 18:26:30 +0100

    initial commit

---
+++ build/precache-manifest.6ee18f958226379840ae3e667f759404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d63d454ba8292399f97bf7bf51c1694",
+    "url": "/To-Do-App-in-React/index.html"
+  },
+  {
+    "revision": "f7fe1bbc972a60581e75",
+    "url": "/To-Do-App-in-React/static/css/main.da07969a.chunk.css"
+  },
+  {
+    "revision": "37d693c0db66880c19a2",
+    "url": "/To-Do-App-in-React/static/js/2.b1a81836.chunk.js"
+  },
+  {
+    "revision": "f7fe1bbc972a60581e75",
+    "url": "/To-Do-App-in-React/static/js/main.564f28d9.chunk.js"
+  },
+  {
+    "revision": "759cf7b4cf17cf63d488",
+    "url": "/To-Do-App-in-React/static/js/runtime~main.60884708.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-App-in-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
